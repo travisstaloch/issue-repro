da66bf9e762ab98a3580dd0eb1338dc0db6e4759
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2023-10-11 23:55:16 -0700

    error: expected paths expression to be a struct

---
+++ build.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "issue-repro",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+}
